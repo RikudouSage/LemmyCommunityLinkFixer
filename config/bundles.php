da3ffdf969db158c963fc3c1592f0fad3524d880
Dominik Chrastecky <dominik@chrastecky.cz>
2023-07-22 14:04:59 +0200

    Only use Monolog in dev

--- config/bundles.php
+++ config/bundles.php
@@ -5,6 +5,6 @@ return [
     Rikudou\DynamoDbCacheBundle\RikudouDynamoDbCacheBundle::class => ['prod' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['dev' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
